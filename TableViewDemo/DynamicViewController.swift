5c801ecdcd3ae19934640751fe3597d17b7ae173
Le Ngoc Duy <askbills@gmail.com>
2016-05-18 08:20:31 +0700

    - update set datasource for tableView

--- TableViewDemo/DynamicViewController.swift
+++ TableViewDemo/DynamicViewController.swift
@@ -11,9 +11,31 @@ import UIKit
 class DynamicViewController: UIViewController {
 
 	@IBOutlet var tableView: UITableView!
+	var projects = [[String]]()
 	
 	override func viewDidLoad() {
 		super.viewDidLoad()
+		
+		projects.append(["Project 1: Storm Viewer", "Constants and variables, UITableView, UIImageView, NSFileManager, storyboards"])
+		projects.append(["Project 2: Guess the Flag", "@2x and @3x images, asset catalogs, integers, doubles, floats, operators (+= and -=), UIButton, enums, CALayer, UIColor, random numbers, actions, string interpolation, UIAlertController"])
+		projects.append(["Project 3: Social Media", "UIBarButtonItem, UIActivityViewController, the Social framework, NSURL"])
+		projects.append(["Project 4: Easy Browser", "loadView(), WKWebView, delegation, classes and structs, NSURLRequest, UIToolbar, UIProgressView., key-value observing"])
+		projects.append(["Project 5: Word Scramble", "NSString, closures, method return values, booleans, NSRange"])
+		projects.append(["Project 6: Auto Layout", "Get to grips with Auto Layout using practical examples and code"])
+		projects.append(["Project 7: Whitehouse Petitions", "JSON, NSData, UITabBarController"])
+		projects.append(["Project 8: 7 Swifty Words", "addTarget(), enumerate(), countElements(), find(), join(), property observers, range operators."])
+	}
+
+	func makeAttributedString(title title: String, subtitle: String) -> NSAttributedString {
+		let titleAttributes = [NSFontAttributeName: UIFont.preferredFontForTextStyle(UIFontTextStyleHeadline), NSForegroundColorAttributeName: UIColor.purpleColor()]
+		let subtitleAttributes = [NSFontAttributeName: UIFont.preferredFontForTextStyle(UIFontTextStyleSubheadline)]
+		
+		let titleString = NSMutableAttributedString(string: "\(title)\n", attributes: titleAttributes)
+		let subtitleString = NSAttributedString(string: subtitle, attributes: subtitleAttributes)
+		
+		titleString.appendAttributedString(subtitleString)
+		
+		return titleString
 	}
 
 }
@@ -30,13 +52,14 @@ extension DynamicViewController: UITableViewDataSource {
 	}
 	
 	func tableView(tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
-		return 1
+		return projects.count
 	}
 	
 	func tableView(tableView: UITableView, cellForRowAtIndexPath indexPath: NSIndexPath) -> UITableViewCell {
 		let cell = tableView.dequeueReusableCellWithIdentifier("cell", forIndexPath: indexPath)
 		
-		cell.textLabel?.text = "Text \n \n \(indexPath.row)"
+		let project = projects[indexPath.row]
+		cell.textLabel?.attributedText = makeAttributedString(title: project[0], subtitle: project[1])
 		
 		return cell
 	}
